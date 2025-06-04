d31f2ddf6afbc240eeec22c7201a9534cbf7cd06
skmtrd <skmtgame23@gmail.com>
2025-06-04 15:03:12 +0900

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -13,4 +20,5 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+
+module.exports = withPWA(nextConfig);
