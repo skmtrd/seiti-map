7711880bf385173424b8466f7953ba6be225625a
skmtrd <skmtgame23@gmail.com>
2025-06-05 18:26:43 +0900

    スポット詳細ページにSpacerコンポーネントを追加し、レイアウトを調整

--- src/app/[spotId]/page.tsx
+++ src/app/[spotId]/page.tsx
@@ -1,6 +1,7 @@
 import { checkAuth } from "@/app/actions/auth";
 import { getSpotDetail } from "@/app/actions/spot";
 import { PageBackButton } from "@/components/common/PageBackButton";
+import { Spacer } from "@/components/common/Spacer";
 import { SpotDetailCard } from "@/components/spots/SpotDetailCard";
 
 export default async function SpotDetailPage({
@@ -16,6 +17,7 @@ export default async function SpotDetailPage({
 
   return (
     <div className="container mx-auto px-4 py-8 max-w-2xl">
+      <Spacer height={10} />
       <SpotDetailCard spot={spot} />
       <PageBackButton />
     </div>
