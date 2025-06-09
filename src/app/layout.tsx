e81ea47e4d1a02d01d23adf1f5e02fa44c0a525f
skmtrd <skmtgame23@gmail.com>
2025-06-10 02:13:08 +0900

    toploaderの実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import NextTopLoader from "nextjs-toploader";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NextTopLoader />
         <Toaster position="top-center" />
         <NuqsAdapter>{children}</NuqsAdapter>
       </body>
